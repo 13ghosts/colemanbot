92f332098a7ba9e30fee51f536bc3fa3737e6c08
Jordan Tinney <50788938+13ghosts@users.noreply.github.com>
2023-03-05 19:03:26 -0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,28 @@
+# colemanbot.py
+
+import os
+import discord
+import random
+
+from dotenv import load_dotenv
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+load_dotenv()
+client = discord.Client(intents=intents)
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+@client.event
+async def on_message(self, message):
+    if message.author == self.user:
+        return
+
+
+@client.event
+async def on_message(message):
+    if client.user.mentioned_in(message):
+        await message.channel.send(random.choice(list(open('quotes.txt'))))
+    
+client.run(TOKEN)
